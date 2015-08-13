41a41a006246defcc7ab1dcc2308fac81a066ca2
chitra atmakuru & aimee reiss <student@epicodus.com>
2015-08-13 15:22:48 -0700

    Add Integrationtesting and css

--- src/main/java/CoinCombinations.java
+++ src/main/java/CoinCombinations.java
@@ -1,6 +1,36 @@
 import java.util.Arrays;
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+import java.util.Random;
+
 public class CoinCombinations {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
+    staticFileLocation("/public");
+
+
+get("/", (request, response) -> {
+  Map<String, Object> model = new HashMap<String, Object>();
+  model.put("template", "templates/home.vtl");
+  return new ModelAndView(model, layout);
+}, new VelocityTemplateEngine());
+
+get("/results", (request, response) -> {
+  Map<String, Object> model = new HashMap<String, Object>();
+  model.put("template", "templates/results.vtl");
+
+  Integer input = Integer.parseInt(request.queryParams("userInput"));
+  Integer[] output = numberOfCoins(input);
+  model.put("output", output);
+
+  return new ModelAndView(model, layout);
+}, new VelocityTemplateEngine());
+
+
+  }
 
     public static Integer[] numberOfCoins( Integer userInput) {
 
